932a4fc88b8df45ddf7abe42bbcd3c21b1db1a88
bercikr <rob.bercik@gmail.com>
2015-05-11 08:00:01 -0400

    adding leftovers

---
+++ src/main/java/com/googlecode/amazoncxf/config/AwsConfig.java
@@ -0,0 +1,16 @@
+package com.googlecode.amazoncxf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by Rob on 5/5/2015.
+ */
+@Configuration
+@ImportResource("applicationContext.xml")
+public class AwsConfig
+{
+
+
+
+}
